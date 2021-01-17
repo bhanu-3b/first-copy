b9af8ed388f07dd00923e58d23562dbf893b83ad
bhanu-3b <asbabuchintu@gmail.com>
2021-01-17 17:41:22 +0530

    second verson of sonpy

--- son.py
+++ son.py
@@ -1,1 +1,5 @@
-print("dad")
+call=input("what you called your father:")
+if call=="dad":
+    print("correct son caled me ",call)
+else:
+    print("ooohhh...son called me you are not my dad you are ",call)
